7145133fd1c993afa8303babb4a4f251ab58990a
Ivana Arbutina <ivana.arbutina2@gmail.com>
2024-01-17 23:50:07 +0100

    removing the list of users and adding a new one

--- src/components/datatable/datatable.tsx
+++ src/components/datatable/datatable.tsx
@@ -1,20 +1,23 @@
 import "./datatable.scss";
 import { DataGrid } from "@mui/x-data-grid";
 import { Link } from "react-router-dom";
+
 import { useEffect, useState } from "react";
 import { collection, deleteDoc, doc, onSnapshot } from "firebase/firestore";
 import { db } from "../../firebase";
+import { UserType, userColumns } from "../../datatablesource";
+import { GridColDef, GridRenderCellParams } from "@mui/x-data-grid";
 
 const DataTable = () => {
-  const [data, setData] = useState<DataType[]>([]);
+  const [data, setData] = useState<UserType[]>([]);
 
   useEffect(() => {
     const unsub = onSnapshot(
       collection(db, "users"),
       (snapShot) => {
-        let list: DataType[] = [];
+        let list: UserType[] = [];
         snapShot.docs.forEach((doc) => {
-          list.push({ id: doc.id, ...doc.data() } as DataType);
+          list.push({ id: doc.id, ...doc.data() } as UserType);
         });
         setData(list);
       },
@@ -22,6 +25,7 @@ const DataTable = () => {
         console.log(error);
       }
     );
+    console.log(data);
 
     return () => {
       unsub();
@@ -37,12 +41,12 @@ const DataTable = () => {
     }
   };
 
-  const actionColumn = [
+  const actionColumn: GridColDef<UserType>[] = [
     {
       field: "action",
       headerName: "Action",
       width: 200,
-      renderCell: (params: { row?: { id: number } }) => {
+      renderCell: (params: GridRenderCellParams) => {
         return (
           <div className="cellAction">
             <Link to="/users/test" style={{ textDecoration: "none" }}>
@@ -50,7 +54,7 @@ const DataTable = () => {
             </Link>
             <div
               className="deleteButton"
-              onClick={() => handleDelete(params.row?.id)}
+              onClick={() => handleDelete(params.row?.id as number)}
             >
               Delete
             </div>
@@ -72,7 +76,7 @@ const DataTable = () => {
         <DataGrid
           className="datagrid"
           rows={data}
-          columns={actionColumn} // Assuming `userColumns` is not needed here
+          columns={[...userColumns, ...actionColumn]}
           initialState={{
             pagination: {
               paginationModel: { page: 0, pageSize: 5 },
@@ -87,9 +91,3 @@ const DataTable = () => {
 };
 
 export default DataTable;
-
-// Replace `YourType` with the actual type of your data
-type DataType = {
-  id: number | string;
-  // Other properties...
-};
